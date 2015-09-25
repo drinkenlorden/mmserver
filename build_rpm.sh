31f0a2816b53ad051de86149dc2928851485f2fb
drinkenlorden <drinkenlorden@drinkenlorden>
2015-09-25 12:05:46 +0300

    minor changes in build script

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-RPMBUILD_DIR=`rpm --eval %{_topdir}`
+RPMBUILD_DIR=$(rpm --eval %{_topdir})
 
-pushd `dirname $0` > /dev/null
-MMSERVER_RPM_DIR=$(pwd)
+pushd $(dirname $0) > /dev/null
+RPM_DIR=$(pwd)
 popd > /dev/null
 
 cd $RPMBUILD_DIR/SPECS
-cp $MMSERVER_RPM_DIR/*.spec .
+cp $RPM_DIR/*.spec .
 cd -
 
 spectool -g -R $RPMBUILD_DIR/SPECS/mmserver.spec
